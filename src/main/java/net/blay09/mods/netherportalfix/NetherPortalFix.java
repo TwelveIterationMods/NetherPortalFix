f7732b93643c1e2b5806a8b6d3043a5d5e7dd7f1
blay09 <blay09@hotmail.de>
2020-10-30 17:26:02 +0100

    Fix NetherPortalFix showing up as required on client-side
    #39

--- src/main/java/net/blay09/mods/netherportalfix/NetherPortalFix.java
+++ src/main/java/net/blay09/mods/netherportalfix/NetherPortalFix.java
@@ -24,8 +24,12 @@ import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -45,6 +49,9 @@ public class NetherPortalFix {
     private static final String TO_DIM = "ToDim";
 
     public NetherPortalFix() {
+        // clientSideOnly = true was cooler
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
