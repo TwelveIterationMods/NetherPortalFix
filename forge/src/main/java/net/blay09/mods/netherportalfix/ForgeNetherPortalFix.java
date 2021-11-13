dfa160ca0e77b2f804c5372852683b2f96c9aff9
blay09 <blay09@hotmail.de>
2021-11-14 00:09:15 +0100

    server-only

--- forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
+++ forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
@@ -1,13 +1,18 @@
 package net.blay09.mods.netherportalfix;
 
 import net.blay09.mods.balm.api.Balm;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod(NetherPortalFix.MOD_ID)
 public class ForgeNetherPortalFix {
 
     public ForgeNetherPortalFix() {
         Balm.initialize(NetherPortalFix.MOD_ID, NetherPortalFix::initialize);
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
