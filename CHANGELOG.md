cd0ba8a6868955aeda86f16d5827007ffe7388d6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-09-26 15:10:45 +0200

    feat: Updated to Minecraft 1.20.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,1 @@
-- Updated to Minecraft 1.20
+- Updated to Minecraft 1.20.2
--- forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
+++ forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
@@ -4,7 +4,6 @@ import net.blay09.mods.balm.api.Balm;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(NetherPortalFix.MOD_ID)
 public class ForgeNetherPortalFix {
@@ -12,7 +11,7 @@ public class ForgeNetherPortalFix {
     public ForgeNetherPortalFix() {
         Balm.initialize(NetherPortalFix.MOD_ID, NetherPortalFix::initialize);
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
