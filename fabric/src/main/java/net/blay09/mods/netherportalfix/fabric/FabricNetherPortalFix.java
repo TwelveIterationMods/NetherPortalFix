0962cdd9a103ea29c6ad01b0c169603c8490043c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-08 14:51:01 +0100

    fix(fabric): Fix startup crash on Fabric

--- fabric/src/main/java/net/blay09/mods/netherportalfix/fabric/FabricNetherPortalFix.java
+++ fabric/src/main/java/net/blay09/mods/netherportalfix/fabric/FabricNetherPortalFix.java
@@ -1,6 +1,7 @@
-package net.blay09.mods.netherportalfix;
+package net.blay09.mods.netherportalfix.fabric;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.netherportalfix.NetherPortalFix;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricNetherPortalFix implements ModInitializer {
