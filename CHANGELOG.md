d7896a5453a6551ec0ff01983e8bb5a0697b3688
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-19 16:43:59 +0200

    feat: Update to Minecraft 1.20.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
+Updated to Minecraft 1.20.6
+
 - Updated to Minecraft 1.20.4
--- fabric/src/main/java/net/blay09/mods/netherportalfix/fabric/FabricNetherPortalFix.java
+++ fabric/src/main/java/net/blay09/mods/netherportalfix/fabric/FabricNetherPortalFix.java
@@ -1,12 +1,13 @@
 package net.blay09.mods.netherportalfix.fabric;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.netherportalfix.NetherPortalFix;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricNetherPortalFix implements ModInitializer {
     @Override
     public void onInitialize() {
-        Balm.initialize(NetherPortalFix.MOD_ID, NetherPortalFix::initialize);
+        Balm.initialize(NetherPortalFix.MOD_ID, EmptyLoadContext.INSTANCE, NetherPortalFix::initialize);
     }
 }
