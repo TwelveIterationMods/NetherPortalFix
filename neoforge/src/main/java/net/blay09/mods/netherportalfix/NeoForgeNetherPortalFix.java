eda83f2701910b65e09d4f23414912ab63d00e8e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 17:42:59 +0200

    fix(1.20.6): Fix NeoForge compile

--- neoforge/src/main/java/net/blay09/mods/netherportalfix/NeoForgeNetherPortalFix.java
+++ neoforge/src/main/java/net/blay09/mods/netherportalfix/NeoForgeNetherPortalFix.java
@@ -1,6 +1,8 @@
 package net.blay09.mods.netherportalfix;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
@@ -8,10 +10,9 @@ import net.neoforged.fml.common.Mod;
 @Mod(NetherPortalFix.MOD_ID)
 public class NeoForgeNetherPortalFix {
 
-    public NeoForgeNetherPortalFix() {
-        Balm.initialize(NetherPortalFix.MOD_ID, NetherPortalFix::initialize);
-
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+    public NeoForgeNetherPortalFix(IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modEventBus);
+        Balm.initialize(NetherPortalFix.MOD_ID, context, NetherPortalFix::initialize);
     }
 
 }
