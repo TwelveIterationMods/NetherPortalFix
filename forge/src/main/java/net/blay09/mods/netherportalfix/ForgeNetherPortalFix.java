621d0ddb8e2db42b78a33ff33bd3085e20eb92cb
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-15 13:33:13 +0100

    fix(1.21.11): Fix wrong import

--- forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
+++ forge/src/main/java/net/blay09/mods/netherportalfix/ForgeNetherPortalFix.java
@@ -1,6 +1,6 @@
 package net.blay09.mods.netherportalfix;
 
-import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.forge.platform.runtime.ForgeLoadContext;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.common.Mod;
