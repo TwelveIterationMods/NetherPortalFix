2a5f83b694605ba3b9cdc9a7d924f7060368c922
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 15:55:31 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/netherportalfix/NeoForgeNetherPortalFix.java
+++ neoforge/src/main/java/net/blay09/mods/netherportalfix/NeoForgeNetherPortalFix.java
@@ -3,13 +3,14 @@ package net.blay09.mods.netherportalfix;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(NetherPortalFix.MOD_ID)
 public class NeoForgeNetherPortalFix {
 
-    public NeoForgeNetherPortalFix(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeNetherPortalFix(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(NetherPortalFix.MOD_ID, context, NetherPortalFix::initialize);
     }
 
