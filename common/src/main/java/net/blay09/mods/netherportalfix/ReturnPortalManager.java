16d050e75b1c832c063fcc6c48274f1479acc1ac
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-27 10:02:32 +0100

    refactor: Remove jetbrains annotations

--- common/src/main/java/net/blay09/mods/netherportalfix/ReturnPortalManager.java
+++ common/src/main/java/net/blay09/mods/netherportalfix/ReturnPortalManager.java
@@ -16,7 +16,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.portal.PortalForcer;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 import java.util.UUID;
 
